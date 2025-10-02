e09cf081db73d8438993e9ac8dc9f80286a9bee4
wd-standard-ui <wd-standard@bwd.ac.th>
2025-10-02 16:17:59 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 // config.js
 // ===== ตั้งค่า Supabase =====
 // 👉 แทนที่ค่าด้านล่างนี้ด้วยของจริงของพี่ที่รัก
-export const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY";
+export const SUPABASE_URL = "https://pdxpnneyhpxodtxexxry.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkeHBubmV5aHB4b2R0eGV4eHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzOTQ2ODksImV4cCI6MjA3NDk3MDY4OX0.Z25HJZD--Xr-OR4HEbj6UFcQ1_2FOWWTdK8q1Snu1gM";
 
 // mapping สีวิชา
 export const SUBJECT_COLORS = {
